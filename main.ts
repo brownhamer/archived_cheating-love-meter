074464c5ab68db1b0ec43a40faf5f9a27acd8715
Hendrik <161131951+brownhamer@users.noreply.github.com>
2024-05-24 17:24:27 +0200

    Single digit random

--- main.ts
+++ main.ts
@@ -1,18 +1,28 @@
-let max = 0
+let num = 0
+let prv = 9
 let min = 0
-min = 0
-max = 99
+let max = 9
+let str = "="
 
 input.onButtonPressed(Button.A, function () {
     min = 0
-    max = 49
+    max = 4
+    str = "-"
 })
 
 input.onButtonPressed(Button.B, function () {
-    min = 50
-    max = 99
+    min = 5
+    max = 9
+    str = "+"
 })
 
-input.onPinPressed(TouchPin.P0, function () {
-    basic.showNumber(randint(min, max))
+basic.forever(function () {
+    num = randint(min, max)
+    if (num == prv) {
+        basic.showString(str)
+    }
+    else {
+        basic.showNumber(num)
+        prv = num
+    }
 })
