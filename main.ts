fec3e1b00f31c1c50e1a964eebde99dce97a27be
Hendrik <161131951+brownhamer@users.noreply.github.com>
2024-05-24 13:35:53 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(randint(min, max))
+})
+input.onButtonPressed(Button.A, function () {
+    min = 0
+    max = 49
+})
+input.onButtonPressed(Button.B, function () {
+    min = 50
+    max = 99
+})
+let max = 0
+let min = 0
+min = 0
+max = 99
+basic.forever(function () {
+	
+})
